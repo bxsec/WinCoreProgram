3bffe87df191c5c64ef3815d1cb40f818faf5049
bxsec <bxsecx@163.com>
2017-07-26 07:12:06 +0800

    1.ErrorShow Com

--- 1.ErrShow/1.ErrShow/ErrorShow.cpp
+++ 1.ErrShow/1.ErrShow/ErrorShow.cpp
@@ -1,5 +1,6 @@
 
 #include <Windows.h>
+#include <Windowsx.h>
 #include <tchar.h>
 #include "Resource.h"
 
@@ -31,6 +32,9 @@ INT_PTR WINAPI Dlg_Proc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
 	switch (uMsg) {
 	case WM_INITDIALOG:
+	{
+		return Dlg_OnInitDialog(hwnd, (HWND)wParam, lParam);
+	}
 		break;
 	case WM_COMMAND:
 	{
@@ -41,7 +45,12 @@ INT_PTR WINAPI Dlg_Proc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	}
 		break;
 	case ESM_POKECODEANDLOOKUP:
-
+	{
+		SetDlgItemInt(hwnd, IDC_ERRORCODE, (UINT)wParam, FALSE);
+		PostMessage(hwnd, WM_COMMAND, MAKEWPARAM((UINT)(int)(IDOK), (UINT)(BN_CLICKED)),
+			(LPARAM)(HWND)(GetDlgItem(hwnd, IDOK)));
+		SetForegroundWindow(hwnd);
+	}
 		break;
 	}
 
@@ -51,11 +60,76 @@ INT_PTR WINAPI Dlg_Proc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 
 BOOL Dlg_OnInitDialog(HWND hwnd, HWND hwndFocus, LPARAM lParam)
 {
+	// Set the dialog box icons
+	//chSETDLGICONS(hwnd, IDI_ERRORSHOW);
+	SendMessage(hwnd, WM_SETICON, ICON_BIG, (LPARAM)
+		LoadIcon((HINSTANCE)GetWindowLongPtr(hwnd, GWLP_HINSTANCE),
+			MAKEINTRESOURCE(IDI_ERRORSHOW)));
+	SendMessage(hwnd, WM_SETICON, ICON_SMALL, (LPARAM)
+		LoadIcon((HINSTANCE)GetWindowLongPtr(hwnd, GWLP_HINSTANCE),
+			MAKEINTRESOURCE(IDI_ERRORSHOW)));
+
+	//设置错误码不超过5位
+	//Edit_LimitText(GetDlgItem(hwnd, IDC_ERRORCODE), 5);
+	SendMessage(GetDlgItem(hwnd, IDC_ERRORCODE), EM_LIMITTEXT, (WPARAM)(5), 0);
+
+	SendMessage(hwnd, ESM_POKECODEANDLOOKUP, lParam, 0);
 
 	return TRUE;
 }
 
 void Dlg_OnCommand(HWND hwnd, int id, HWND hwndCtl, UINT codeNotify)
 {
+	switch (id)
+	{
+	case IDCANCEL:
+		EndDialog(hwnd, id);
+		break;
+	case IDC_ALWAYSONTOP:
+		SetWindowPos(hwnd, IsDlgButtonChecked(hwnd, IDC_ALWAYSONTOP) ? HWND_TOPMOST : HWND_NOTOPMOST, 0, 0,
+			0, 0, SWP_NOMOVE | SWP_NOSIZE);
+		break;
 
+	case IDC_ERRORCODE:
+		EnableWindow(GetDlgItem(hwnd, IDOK), GetWindowTextLength(hwndCtl) > 0);
+		break;
+
+	case IDOK:
+		//获取错误码
+		DWORD dwError = GetDlgItemInt(hwnd, IDC_ERRORCODE, NULL, FALSE);
+		HLOCAL hlocal = NULL;
+
+		DWORD systemLocale = MAKELANGID(LANG_NEUTRAL, SUBLANG_NEUTRAL);
+
+		BOOL fOk = FormatMessage(
+			FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS |
+			FORMAT_MESSAGE_ALLOCATE_BUFFER,
+			NULL, dwError, systemLocale,
+			(PTSTR)&hlocal, 0, NULL
+		);
+
+		if (!fOk) {
+			HMODULE hDll = LoadLibraryEx(TEXT("netmsg.dll"), NULL,
+				DONT_RESOLVE_DLL_REFERENCES);
+
+			if (hDll != NULL) {
+				fOk = FormatMessage(
+					FORMAT_MESSAGE_FROM_HMODULE | FORMAT_MESSAGE_IGNORE_INSERTS |
+					FORMAT_MESSAGE_ALLOCATE_BUFFER,
+					hDll, dwError, systemLocale,
+					(PTSTR)&hlocal, 0, NULL);
+				FreeLibrary(hDll);
+			}
+		}
+		if (fOk && (hlocal != NULL)) {
+			SetDlgItemText(hwnd, IDC_ERRORTEXT, (PCTSTR)LocalLock(hlocal));
+			LocalFree(hlocal);
+		}
+		else {
+			SetDlgItemText(hwnd, IDC_ERRORTEXT,
+				TEXT("No text found for this error number."));
+		}
+		break;
+
+	}
 }
